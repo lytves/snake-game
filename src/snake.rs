3eac08647507b6f40f8ab80a9ac1c4d85f5ac3f0
lytves <22356770+lytves@users.noreply.github.com>
2020-12-06 21:13:51 +0100

    draw & snake principal functions created

--- src/snake.rs
+++ src/snake.rs
@@ -38,7 +38,7 @@ pub struct Snake {
 }
 
 impl Snake {
-    pub fn new(x: i32, y: 32) -> Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
         let mut body: LinkedList<Block> = LinkedList::new();
         body.push_back(Block {
             x: x + 2,
